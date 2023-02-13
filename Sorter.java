3a944d5d07d88445ba7315414f962f79a0dcafad
AimRAS <118935291+AimRAS@users.noreply.github.com>
2023-02-13 11:41:14 +0300

    test3

--- Sorter.java
+++ Sorter.java
@@ -1,7 +1,9 @@
+
 public class Sorter {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("Test2");
+        System.out.println("test");
     }
 }
 
